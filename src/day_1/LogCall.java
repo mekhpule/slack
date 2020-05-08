490607ae8da223e91c7f235be1a4140b791c55fc
mekhpule <64181630+mekhpule@users.noreply.github.com>
2020-05-08 15:35:41 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -3,6 +3,9 @@ package day_1;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("This is a new class LogCall");
+        System.out.println("Let push the class to Inteleji");
+        
+        
 
     }
 }
